06d838ef7874d92bd92d92d1355e0f3bfb1dbb81
Ian Purton <ian.purton@gmail.com>
2014-10-01 13:44:00 +0200

    Sweeper spec

--- lib/onchain/sweeper.rb
+++ lib/onchain/sweeper.rb
@@ -4,7 +4,7 @@ class OnChain
     # With a bunch of HD wallet paths, build a transaction
     # That pays all the coins to a certain address
     def sweep
-      puts 'hello'
+      return 'hello'
     end
   end
 end
---
+++ spec/lib/sweeper_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe OnChain do
+  it "should say hello" do
+    OnChain.sweep == 'hello'
+  end
+end
