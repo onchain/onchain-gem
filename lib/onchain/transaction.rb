3c65d4d0dce2590d4f0ffc5e37c3ea773f839736
Ian Purton <ian.purton@gmail.com>
2014-11-17 10:56:53 +0100

    Multi sig tx creation and signing.

--- lib/onchain/transaction.rb
+++ lib/onchain/transaction.rb
@@ -1,6 +1,20 @@
 class OnChain::Transaction
   class << self
     
+    # Given a send address and an amount produce a transaction 
+    # and a list of hashes that need to be signed.
+    # 
+    # The transaction will be in hex format.
+    #
+    # The list of hashes that need to be signed will be in this format
+    #
+    # [input index]{public_key => { :hash => hash} }
+    #
+    # i.e.
+    #
+    # [0][034000....][:hash => '345435345...'] 
+    # [0][02fee.....][:hash => '122133445....']
+    # 
     def create_transaction(redemption_scripts, address, amount_in_satoshi, miners_fee)
     
       tx = Bitcoin::Protocol::Tx.new
@@ -19,7 +33,6 @@ class OnChain::Transaction
       tx = Bitcoin::Protocol::Tx.new
       
       # Process the unpsent outs.
-      hashes = []
       unspents.each_with_index do |spent, index|
 
         script = redemption_scripts[indexes[index]]
@@ -27,14 +40,6 @@ class OnChain::Transaction
         txin = Bitcoin::Protocol::TxIn.new([ spent[0] ].pack('H*').reverse, spent[1])
         txin.script_sig = OnChain::hex_to_bin(script)
         tx.add_in(txin)
-        
-        hash = tx.signature_hash_for_input(tx.in.count - 1, OnChain::hex_to_bin(script), 1)
-        
-        rs_script = Bitcoin::Script.new OnChain::hex_to_bin(script)
-        rs_script.get_multisig_pubkeys.each do |key|
-          hashes << OnChain.bin_to_hex(key) +  ':' + OnChain::bin_to_hex(hash)
-        end
-        
       end
       
       # Do we have enough in the fund.
@@ -57,21 +62,60 @@ class OnChain::Transaction
   
         tx.add_out(txout)
       end
+
+      inputs_to_sign = []
+      tx.in.each_with_index do |txin, index|
+        hash = tx.signature_hash_for_input(index, txin.script, 1)
+        
+        rsscript = Bitcoin::Script.new txin.script
+        rsscript.get_multisig_pubkeys.each do |key|
+          
+          if inputs_to_sign[index] == nil
+            inputs_to_sign[index] = {}
+          end
+          inputs_to_sign[index][OnChain.bin_to_hex(key)] = {:hash => OnChain::bin_to_hex(hash)}
+        end
+      end
     
-      return OnChain::bin_to_hex(tx.to_payload), hashes
+      return OnChain::bin_to_hex(tx.to_payload), inputs_to_sign
     end
     
-    def signTransaction(transaction_hex, sig_list)
+    # Given a transaction in hex string format, apply
+    # the given signature list to it.
+    #
+    # Signatures should be in the format
+    #
+    # [0]{034000.....' => {:hash => '345435345....', :sig => '435fgdf4553...'}}
+    # [0]{02fee.....' => {:hash => '122133445....', :sig => '435fgdf4553...'}}
+    #
+    def sign_transaction(transaction_hex, sig_list)
       
       tx = Bitcoin::Protocol::Tx.new OnChain::hex_to_bin(transaction_hex)
       
+      tx.in.each_with_index do |txin, index|
+        
+        hash = OnChain.bin_to_hex(tx.signature_hash_for_input(index, txin.script, 1))
+        
+        sigs = []
+        
+        rscript = Bitcoin::Script.new txin.script
+        rscript.get_multisig_pubkeys.each do |key|
+          
+          hkey = OnChain.bin_to_hex(key)
+          if sig_list[index][hkey] != nil and sig_list[index][hkey][:sig] != nil
+            
+            # Add the signature to the list.
+            sigs << OnChain.hex_to_bin(sig_list[index][hkey][:sig])
+            
+          end
+        end
+        
+        if sigs.count > 0
+          txin.script = Bitcoin::Script.to_p2sh_multisig_script_sig(rscript.to_payload, sigs)
+        end
+      end
       
-      
-    end
-    
-    private
-    
-    def get_public_keys_from_redemption_script
+      return OnChain::bin_to_hex(tx.to_payload)
     end
   end
 end
--- spec/lib/transaction_spec.rb
+++ spec/lib/transaction_spec.rb
@@ -7,21 +7,21 @@ describe OnChain do
   
   it "should create a valid transaction" do
     
-    tx, hashes = OnChain::Transaction.create_transaction(
+    tx, inputs_to_sign = OnChain::Transaction.create_transaction(
       REDEMPTION_SCRIPTS, 
       '1STRonGxnFTeJiA7pgyneKknR29AwBM77', 11366, 10000)
       
     expect(tx).to eq("01000000029fd77c01b4f81f142e7e066eb9abeb4952ec5fdea51036acbb22b5ffeb57fd5f0100000047522102fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc4210396e42d3c584da0300ee44dcbaee0eccaa0e6ae2264fdd2554af6d2953f95bf9952aeffffffffc0161c6d62ac75f36bf95fcb2a8222f2274e86c2dcaec3434a0b6b6e0a6b60800000000047522102fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc421034000cea8f9cbaf88095d3ef539ee438e3cefea9ed9585e2e182b45496f071a8352aeffffffff01662c0000000000001976a91404d075b3f501deeef5565143282b6cfe8fad5e9488ac00000000")
   
-    expect(hashes[0]).to eq('02fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc4:07c5574ae48786421ad1f3e2d58eaf485c87ba83f134274465f503c0ec689c8a')
-    expect(hashes[1]).to eq('0396e42d3c584da0300ee44dcbaee0eccaa0e6ae2264fdd2554af6d2953f95bf99:07c5574ae48786421ad1f3e2d58eaf485c87ba83f134274465f503c0ec689c8a')
-    expect(hashes[2]).to eq('02fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc4:2fbab2b0240688630e50f38f7e3bed02382a7d38323a37f75230e27c0cbc6732')
-    expect(hashes[3]).to eq('034000cea8f9cbaf88095d3ef539ee438e3cefea9ed9585e2e182b45496f071a83:2fbab2b0240688630e50f38f7e3bed02382a7d38323a37f75230e27c0cbc6732')
+    expect(inputs_to_sign[0]['02fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc4'][:hash]).to eq('355ab58049169af3ab36486e0b8251279027f5c0e195422fcd25c36668d3c0e7')
+    expect(inputs_to_sign[0]['0396e42d3c584da0300ee44dcbaee0eccaa0e6ae2264fdd2554af6d2953f95bf99'][:hash]).to eq('355ab58049169af3ab36486e0b8251279027f5c0e195422fcd25c36668d3c0e7')
+    expect(inputs_to_sign[1]['02fd89e243d38f4e24237eaac4cd3a6873ce45aa4036ec0c7b79a4d4ac0fefebc4'][:hash]).to eq('f14b88c7d42f10e23f307fd14ee997870e8f8825d0fc2092be5aabd8eec81735')
+    expect(inputs_to_sign[1]['034000cea8f9cbaf88095d3ef539ee438e3cefea9ed9585e2e182b45496f071a83'][:hash]).to eq('f14b88c7d42f10e23f307fd14ee997870e8f8825d0fc2092be5aabd8eec81735')
   end
   
   it "should create a partially signed transaction" do
     
-    tx, hashes = OnChain::Transaction.create_transaction(
+    tx, inputs_to_sign = OnChain::Transaction.create_transaction(
       REDEMPTION_SCRIPTS, 
       '1STRonGxnFTeJiA7pgyneKknR29AwBM77', 11366, 10000)
       
@@ -29,28 +29,26 @@ describe OnChain do
       
       node = MoneyTree::Node.from_serialized_address HANDY_KEY
       
-      signed_hashes = []
-      
-      hashes.each do |hash|
+      inputs_to_sign.each do |input|
         
-        if hash.split(':')[0] == node.public_key.to_hex
+        if input[node.public_key.to_hex] != nil
+          
           
           wif = node.private_key.to_hex
           pk = Bitcoin::Key.new wif
           
-          hash_to_sign = hash.split(':')[1]
+          hash_to_sign = input[node.public_key.to_hex][:hash]
           
           sig =  OnChain.bin_to_hex(pk.sign(OnChain.hex_to_bin(hash_to_sign)))
           
-          signed_hashes << hash + ":" + sig
-        else
-          signed_hashes << hash
+          input[node.public_key.to_hex][:sig] = sig
         end
+          
         
       end
       
-      OnChain::Transaction.sign_transaction(tx, signed_hashes)
-
-      puts signed_hashes
+      tx_signed = OnChain::Transaction.sign_transaction(tx, inputs_to_sign)
+      
+      expect(tx_signed.length).to be > tx.length
     end
 end
