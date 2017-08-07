ebec83226cab4d45e790dc3ab26deb99ac901597
John Doe <ian.purton+c9@gmail.com>
2017-08-07 06:26:44 +0000

    Add some bitocin cash tests.

--- spec/lib/bitcoin_cash_spec.rb
+++ spec/lib/bitcoin_cash_spec.rb
@@ -7,7 +7,7 @@ describe OnChain do
     @bitcoin = OnChain::BlockChain::COINS[:bitcoin][:apis].first[:provider]
   end
   
-  it "should generate the correct address format" do
+  it "should match the old blockchain for coins that haven't moved." do
       
     # This is an old no lobger used address so the results should
     # be the same form both networks.
@@ -20,6 +20,22 @@ describe OnChain do
     
     expect(test1[0][:outs].length).to eq(3)
     expect(test2[0][:outs].length).to eq(3)
+    
+  end
+  
+  it "balances should be different for addresses active on bitcoin." do
+      
+    # This is an old no lobger used address so the results should
+    # be the same form both networks.
+    
+    # Insight API
+    test1 =  @bitcoin_cash.get_balance('1STRonGxnFTeJiA7pgyneKknR29AwBM77')
+    
+    # Blockchain API
+    test2 =  @bitcoin.get_balance('1STRonGxnFTeJiA7pgyneKknR29AwBM77')
+    
+    
+    expect(test1).not_to eq(test2)
     
   end
   
