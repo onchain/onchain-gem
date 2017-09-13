232d00be4d335b6094d6d61c99a00834611df09b
John Doe <ian.purton+c9@gmail.com>
2017-09-13 12:10:46 +0000

    Add etherreum exhcnage rate.

--- lib/onchain/exchange_rates.rb
+++ lib/onchain/exchange_rates.rb
@@ -23,6 +23,7 @@ class OnChain::ExchangeRate
               when :bitcoin then 'bitcoin'
               when :zcash then 'zcash'
               when :zclassic then 'zclassic'
+              when :etherum then 'ethereum'
               else nil
             end
             
--- lib/onchain/version.rb
+++ lib/onchain/version.rb
@@ -1,3 +1,3 @@
 module Onchain
-  VERSION = "4.31"
+  VERSION = "4.32"
 end
