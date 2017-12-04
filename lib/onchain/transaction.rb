bc5f84849461610b03fd7fa84726ccddbeb5f6f1
John Doe <ian.purton+c9@gmail.com>
2017-12-04 12:02:09 +0000

    Refactor and fix tests.

--- lib/onchain/transaction.rb
+++ lib/onchain/transaction.rb
@@ -388,9 +388,8 @@ class OnChain::Transaction
         hash = tx.signature_hash_for_input(index, txin.script, 1)
         
         if network == :bitcoin_cash
-          sig_hash = Bitcoin::Protocol::Tx::SIGHASH_TYPE[:forkid] | Bitcoin::Protocol::Tx::SIGHASH_TYPE[:all] 
-          
-          hash = tx.signature_hash_for_cash_input(index, txin.script, unspents[index][3], sig_hash)
+          prev_out = unspents[index][3]
+          hash = tx.signature_hash_for_input(index, txin.script, 1, prev_out, true)
         end
         
         script = Bitcoin::Script.new txin.script
