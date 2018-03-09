ad0bfa614ea5bfc581959721bcf55ba56ae237ab
John Doe <johndoe@example.com>
2018-03-09 09:05:09 +0100

    Ready for test.

--- spec/lib/bitcoin_private_spec.rb
+++ spec/lib/bitcoin_private_spec.rb
@@ -6,65 +6,65 @@ describe OnChain do
      "bitcoin_private_spec/" + example.description
   end
   
-  it "should create a valid payment" do
-    VCR.turned_off do
-    
-      pk = ENV['private_key']
-      public_key  = OnChain::Address.address_from_wif(pk, :bitcoin_private)
-      puts "Public Key we are using - #{public_key}\n"
-      
-      if ! pk
-        throw 'Set private key in env'
-      end
-      
-      key = Bitcoin::Key.from_base58 pk
-      
-      stub_request(:get, "https://explorer.btcprivate.org/api/addr/#{public_key}/utxo").
-         with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'explorer.btcprivate.org', 'User-Agent'=>'Ruby'}).
-         to_return(status: 200, body: '[{"address":"b1H4VhwWAoVGMqnXM8oqsKK2WzpMGkbRamm","txid":"db832d5b046eb2a297d2f3044c22a891d9c4adb9806396e39e1ec2cf531c0375","vout":0,"scriptPubKey":"76a9148a1dde437f546b20222b4a4434c8b9dd4d8b05ba88ac","amount":0.005,"satoshis":500000,"height":272358,"confirmations":9711},{"address":"b1H4VhwWAoVGMqnXM8oqsKK2WzpMGkbRamm","txid":"9f416e184a3e918e96095f27fbb20452bf4cb113cabd514046cdec43c8acccbc","vout":0,"scriptPubKey":"76a9148a1dde437f546b20222b4a4434c8b9dd4d8b05ba88ac","amount":0.005,"satoshis":500000,"height":272353,"confirmations":9716}]', headers: {})
-      
-      tx_hex, inputs_to_sign = OnChain::Transaction.create_single_address_transaction(
-        "#{public_key}", 
-        'b19nP23vNEyWiyz6B9G5hFnt7w12RAPbsU9', 482699, 
-        17000, 'b19nP23vNEyWiyz6B9G5hFnt7w12RAPbsU9', 300, :bitcoin_private)
-        
-      puts "Unsigned TX\n" + tx_hex
-      
-      puts "\nInputs before signing.\n"
-      
-      puts inputs_to_sign
-      
-      sign_with_eckey(inputs_to_sign, key)
-      
-      puts "\nInputs after signing.\n"
-      
-      puts inputs_to_sign
-      
-      puts "\nSigned? " + OnChain::Transaction.do_we_have_all_the_signatures(inputs_to_sign).to_s
-      
-      hash = inputs_to_sign.first[inputs_to_sign.first.keys.first]["hash"]
-      sig = inputs_to_sign.first[inputs_to_sign.first.keys.first]["sig"]
-      
-      
-      # Verify signature
-      verified = Bitcoin.verify_signature([hash].pack("H*"), 
-          [sig].pack("H*"), key.pub)
-          
-      puts "Verified? " + verified.to_s
-      
-    
-      hash_type = Bitcoin::Script::SIGHASH_TYPE[:all]
-      hash_type = hash_type | Bitcoin::Script::SIGHASH_TYPE[:forkid]
-      
-      signed_tx = OnChain::Transaction.sign_transaction(
-        tx_hex, inputs_to_sign, key.pub, hash_type)
-        
-      puts
-      puts "Signed TX"
-      puts signed_tx
-          
-    end
-  end
+  #it "should create a valid payment" do
+  #  VCR.turned_off do
+  #  
+  #    pk = ENV['private_key']
+  #    public_key  = OnChain::Address.address_from_wif(pk, :bitcoin_private)
+  #    puts "Public Key we are using - #{public_key}\n"
+  #    
+  #    if ! pk
+  #      throw 'Set private key in env'
+  #    end
+  #    
+  #    key = Bitcoin::Key.from_base58 pk
+  #    
+  #    stub_request(:get, "https://explorer.btcprivate.org/api/addr/#{public_key}/utxo").
+  #       with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'explorer.btcprivate.org', 'User-Agent'=>'Ruby'}).
+  #       to_return(status: 200, body: '[{"address":"b1H4VhwWAoVGMqnXM8oqsKK2WzpMGkbRamm","txid":"db832d5b046eb2a297d2f3044c22a891d9c4adb9806396e39e1ec2cf531c0375","vout":0,"scriptPubKey":"76a9148a1dde437f546b20222b4a4434c8b9dd4d8b05ba88ac","amount":0.005,"satoshis":500000,"height":272358,"confirmations":9711},{"address":"b1H4VhwWAoVGMqnXM8oqsKK2WzpMGkbRamm","txid":"9f416e184a3e918e96095f27fbb20452bf4cb113cabd514046cdec43c8acccbc","vout":0,"scriptPubKey":"76a9148a1dde437f546b20222b4a4434c8b9dd4d8b05ba88ac","amount":0.005,"satoshis":500000,"height":272353,"confirmations":9716}]', headers: {})
+  #    
+  #    tx_hex, inputs_to_sign = OnChain::Transaction.create_single_address_transaction(
+  #      "#{public_key}", 
+  #      'b19nP23vNEyWiyz6B9G5hFnt7w12RAPbsU9', 482699, 
+  #      17000, 'b19nP23vNEyWiyz6B9G5hFnt7w12RAPbsU9', 300, :bitcoin_private)
+  #      
+  #    puts "Unsigned TX\n" + tx_hex
+  #    
+  #    puts "\nInputs before signing.\n"
+  #    
+  #    puts inputs_to_sign
+  #    
+  #    sign_with_eckey(inputs_to_sign, key)
+  #    
+  #    puts "\nInputs after signing.\n"
+  #    
+  #    puts inputs_to_sign
+  #    
+  #    puts "\nSigned? " + OnChain::Transaction.do_we_have_all_the_signatures(inputs_to_sign).to_s
+  #    
+  #    hash = inputs_to_sign.first[inputs_to_sign.first.keys.first]["hash"]
+  #    sig = inputs_to_sign.first[inputs_to_sign.first.keys.first]["sig"]
+  #    
+  #    
+  #    # Verify signature
+  #    verified = Bitcoin.verify_signature([hash].pack("H*"), 
+  #        [sig].pack("H*"), key.pub)
+  #        
+  #    puts "Verified? " + verified.to_s
+  #    
+  #  
+  #    hash_type = Bitcoin::Script::SIGHASH_TYPE[:all]
+  #    hash_type = hash_type | Bitcoin::Script::SIGHASH_TYPE[:forkid]
+  #    
+  #    signed_tx = OnChain::Transaction.sign_transaction(
+  #      tx_hex, inputs_to_sign, key.pub, hash_type)
+  #      
+  #    puts
+  #    puts "Signed TX"
+  #    puts signed_tx
+  #        
+  #  end
+  #end
   
   
   def sign_with_eckey(inputs_to_sign, pk)
