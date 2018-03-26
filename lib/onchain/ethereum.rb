e904187cf4941ebf9f02b3f7311652734318e87e
John Doe <ian.purton+c9@gmail.com>
2018-03-26 09:32:14 +0000

    Sign the transfer.

--- lib/onchain/ethereum.rb
+++ lib/onchain/ethereum.rb
@@ -50,12 +50,12 @@ class OnChain::Ethereum
     
     ERC20_TRANSFER_ABI = 'transfer(address,uint256)'
     
-    def create_token_transfer(orig_addr, destination_addr, contract_addr, 
+    def create_token_transfer(orig_addr, dest_addr, contract_addr, 
       amount, decimal_places, gas_price = GAS_PRICE, gas_limit = GAS_LIMIT)
       
       nonce = OnChain::BlockChain.get_nonce(orig_addr)
       
-      data = erc20_transfer_data(destination_addr, amount, decimal_places)
+      data = erc20_transfer_data(dest_addr, amount, decimal_places)
 
       tx = Eth::Tx.new({
         data: data,
@@ -73,23 +73,48 @@ class OnChain::Ethereum
 
       return tx.hex, inputs_to_sign
     end
+    
+    def finish_token_transfer(orig_addr, dest_addr, contract_addr, 
+      amount, decimal_places, r, s ,v, 
+      gas_price = GAS_PRICE, gas_limit = GAS_LIMIT)
+  
+      nonce = OnChain::BlockChain.get_nonce(orig_addr)
+      
+      data = erc20_transfer_data(dest_addr, amount, decimal_places)
+      
+      # Reconstruct it and sign it.
+      tx = Eth::Tx.new({
+        data: data,
+        gas_limit: gas_limit,
+        gas_price: gas_price,
+        nonce: nonce,
+        to: dest_addr,
+        value: 0,
+        v: v,
+        s: s.to_i(16),
+        r: r.to_i(16)
+      })
+      
+      return tx
+    
+    end
   
     private 
     
     def erc20_transfer_data(destination_addr, amount, decimal_places)
         
-        # The function selector is the first four bytes of the keccak-256 hash of 
-        # the canonical function signature.
-        function_selector = Eth::Utils.bin_to_hex(
-          Eth::Utils.keccak256 ERC20_TRANSFER_ABI)[0..7]
-  
-        destination_padded = Eth::Utils.remove_hex_prefix destination_addr.downcase  
-        destination_padded = destination_padded.rjust(64, '0')
-        
-        amount_padded_32 = (amount * (10 ** decimal_places)).to_s(16)
-        amount_padded_32 = amount_padded_32.rjust(64, '0')
-        
-        return function_selector + destination_padded + amount_padded_32
+      # The function selector is the first four bytes of the keccak-256 hash of 
+      # the canonical function signature.
+      function_selector = Eth::Utils.bin_to_hex(
+        Eth::Utils.keccak256 ERC20_TRANSFER_ABI)[0..7]
+
+      destination_padded = Eth::Utils.remove_hex_prefix destination_addr.downcase  
+      destination_padded = destination_padded.rjust(64, '0')
+      
+      amount_padded_32 = (amount * (10 ** decimal_places)).to_s(16)
+      amount_padded_32 = amount_padded_32.rjust(64, '0')
+      
+      return function_selector + destination_padded + amount_padded_32
     end
     
   end
--- spec/lib/erc20_spec.rb
+++ spec/lib/erc20_spec.rb
@@ -36,4 +36,25 @@ describe OnChain do
     end
     
   end
+  
+  it "should finalize a token transfer." do
+    
+    VCR.use_cassette(the_subject) do
+      
+      r = '0xbc8914339995ccc9787a2a34090345b349f18cd2a73ae5644996cbb9b5270396'
+      s = '0x11b79f9bc9c95fae0a97bb12be5af1770b370d5498d7f8d7b19489cd922cd67f'
+      v = 28
+      
+      tx = OnChain::Ethereum.finish_token_transfer(
+        '0x46FC2341DC457BA023cF6d60Cb0729E5928A81E6', # from me
+        '0x46FC2341DC457BA023cF6d60Cb0729E5928A81E6', # to someone
+        '0x1175a66a5c3343bbf06aa818bb482ddec30858e0', # this is the contract
+        1, 18,
+        r, s, v) # Amount and decimal places.
+        
+      expect(tx.hex).to eq('0xf8a9808504a817c8008275309446fc2341dc457ba023cf6d60cb0729e5928a81e680b844a9059cbb00000000000000000000000046fc2341dc457ba023cf6d60cb0729e5928a81e60000000000000000000000000000000000000000000000000de0b6b3a76400001ca0bc8914339995ccc9787a2a34090345b349f18cd2a73ae5644996cbb9b5270396a011b79f9bc9c95fae0a97bb12be5af1770b370d5498d7f8d7b19489cd922cd67f')
+      
+    end
+    
+  end
 end
